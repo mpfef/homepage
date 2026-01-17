04f30ed115c703d9ccb200ccf8c6078eb87bc6e6
npfe <nicolas.pfefferle@gmail.com>
2026-01-17 16:06:58 +0100

    Fixed typos.

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,22 +1,22 @@
 export const aboutMarkdown = `
-Infirmière anesthésiste durant 38 ans, les contraintes professionnelles ont rendu les moments d’échanges avec les patients.es de plus en plus succins.
+Infirmière anesthésiste durant 38 ans, les contraintes professionnelles ont rendu les moments d’échanges avec les patients.es de plus en plus succints.
 <br><br>
 
 Moins de temps et plus de stress pour les patients.es et les membres du personnel, cette constatation m’a incité à me former auprès de la HES-SO Valais Wallis en Art et techniques hypnotiques de 2012 à 2014 et obtenu le Certificate of Advenced Studies.
 <br><br>
 
- Le bloc opératoire est un monde technique, bruyant et agressif et j’ai pu grâce au soutien de mes collègues et responsables de service intégrés ces outils pour en faire bénéficier les patients ( accompagnement lors de pose de cathéter, accueil des enfants et des parents, gestion des ressentis douloureux en post opératoire en salle de réveil, accompagnement lors de chirurgie sous anesthésie locale…)
+ Le bloc opératoire est un monde technique, bruyant et agressif et j’ai pu grâce au soutien de mes collègues et responsables de service intégrer ces outils pour en faire bénéficier les patients ( accompagnement lors de pose de cathéter, accueil des enfants et des parents, gestion des ressentis douloureux en post opératoire en salle de réveil, accompagnement lors de chirurgie sous anesthésie locale…)
 <br><br>
 
-J’ai eu l’opportunité d’intégrer le comité pédagogique de ce CAS et durant 10 ans gérer la responsabilité d’un module. Actuellement je continue d’y intervenir lors de cours et de session d’examens.
+J’ai eu l’opportunité d’intégrer le comité pédagogique de ce CAS et durant 10 ans gérer la responsabilité d’un module. Actuellement je continue d’y intervenir lors de cours et de sessions d’examens.
 <br><br>
 
-Membre de Institut Romand d’Hypnose Suisse ( IRHyS), je suis mandatée pour animer des cours d’Initiation à l’auto-hypnose  au quotidien avec deux autres collègues (Martigny).  Je collabore aussi auprès des HUG dans la formation du personnel en hypnose clinique, et d’autres différents mandats.
+Membre de Institut Romand d’Hypnose Suisse (**IRHyS**), je suis mandatée pour animer des cours d’Initiation à l’auto-hypnose  au quotidien avec deux autres collègues (Martigny).  Je collabore aussi auprès des HUG dans la formation du personnel en hypnose clinique, et d’autres différents mandats.
 
 # Mes interventions publiques
-- **Fédération suisse des Infirmières et infirmiers anesthésistes/ publication revue dans le journal d’anesthésie, Anesthésie et hypnose** : technicité et compagnonnage. 02/2013 p.20-p23.
+- **Fédération suisse des Infirmières et infirmiers anesthésistes/ publication revue dans le journal d’anesthésie, Anesthésie et hypnose** : Technicité et Compagnonnage. 02/2013 p.20-p23.
 - Conférence AVADOL sur la place de l’hypnose chez les patients porteurs de syndrome douloureux chronique.
-- **36°9 TSR** : ils chuchotent aux oreilles des patients
-- **Canal 9** : https://canal9.ch/fr/nouvelles-therapies-3-sur-3-hypnose-une-technique-medicale-utile-aussi-pour-les-petites-chirurgies/
+- **36°9 TSR** : Ils chuchotent aux oreilles des patients
+- **Canal 9** : [Nouvelles thérapies (3 sur 3). L’hypnose, une technique médicale utile aussi pour les petites chirurgies](https://canal9.ch/fr/nouvelles-therapies-3-sur-3-hypnose-une-technique-medicale-utile-aussi-pour-les-petites-chirurgies/)
 
 `;
