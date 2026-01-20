14f6dcc178a826dedf4021e96d4faef83648cd6f
npfe <nicolas.pfefferle@gmail.com>
2026-01-20 09:43:17 +0100

    Fixed path for deplyment at address.

--- src/utils/assetPath.ts
+++ src/utils/assetPath.ts
@@ -1,6 +1,6 @@
 export const getAssetPath = (path: string) => {
   const isProd = process.env.NODE_ENV === 'production';
-  const prefix = isProd ? '/homepage' : '';
+  const prefix = isProd ? '' : '';
 
   // Clean up the path to ensure no double slashes
   const cleanPath = path.startsWith('/') ? path : `/${path}`;
